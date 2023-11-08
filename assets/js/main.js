4b3950bc48e756e55a761735694f197d406e4452
moufid-elaroui <moufid.elaroui@gmail.com>
2023-11-08 14:13:57 +0100

    skiplink

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,30 @@
+
+// Sélectionner les éléments du DOM
 let mobileNavBtn = document.getElementById("mobile-nav-btn");
 let mobileNav = document.getElementById("mobile-nav");
+let button = document.getElementById('toggleButton');
+let content = document.getElementById('content');
 
+// Définir une fonction générique pour basculer l'attribut aria-expanded
+function toggleExpanded(element) {
+  var isExpanded = element.getAttribute('aria-expanded') === 'true';
+  element.setAttribute('aria-expanded', !isExpanded);
+}
+
+// Ajouter un écouteur d'événements au bouton de navigation mobile
 mobileNavBtn.addEventListener("click", function () {
-  if (mobileNav.hasAttribute("class")) {
-    mobileNav.removeAttribute("class");
-  } else {
-    mobileNav.setAttribute("class", "active");
-  }
+  // Basculer la classe active sur le menu mobile
+  mobileNav.classList.toggle("active");
+  // Basculer l'attribut aria-expanded sur le bouton de navigation mobile
+  toggleExpanded(mobileNavBtn);
 });
 
-var button = document.getElementById('toggleButton');
-var content = document.getElementById('content');
-
+// Ajouter un écouteur d'événements au bouton de basculement
 button.addEventListener('click', function () {
-  var isExpanded = button.getAttribute('aria-expanded') === 'true';
-  button.setAttribute('aria-expanded', !isExpanded);
-  content.style.display = isExpanded ? 'none' : 'block';
-  // button.textContent = isExpanded ? 'Afficher le contenu' : 'Masquer le contenu';
+  // Basculer l'attribut aria-expanded sur le bouton de basculement
+  toggleExpanded(button);
+  // Basculer l'affichage du contenu
+  content.style.display = content.style.display === 'block' ? 'none' : 'block';
+  // Basculer le texte du bouton de basculement
+  // button.textContent = button.textContent === 'Afficher le contenu' ? 'Masquer le contenu' : 'Afficher le contenu';
 });
